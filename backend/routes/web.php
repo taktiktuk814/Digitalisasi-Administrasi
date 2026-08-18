c297ff6a7886fd7f49cd2732c15f2091fea5499d
taktiktuk814 <mfyulistiawan@gmail.com>
2026-08-18 23:36:54 +0700

    Tambah routing daftar pegawai dan arsip pegawai

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,10 +5,13 @@ use App\Http\Controllers\SuratMasukController;
 use App\Http\Controllers\SuratKeluarController;
 use App\Http\Controllers\AgendaController;
 use App\Http\Controllers\ArsipController;
+use App\Http\Controllers\PegawaiController;
 use Illuminate\Support\Facades\Route;
+
 Route::get('/',[DashboardController::class,'index'])->name('dashboard');
 Route::resource('aset',AsetController::class)->except(['show']);
 Route::resource('surat-masuk',SuratMasukController::class)->except(['show']);
 Route::resource('surat-keluar',SuratKeluarController::class)->except(['show']);
 Route::resource('agenda',AgendaController::class)->except(['show']);
 Route::resource('arsip',ArsipController::class)->except(['show']);
+Route::resource('pegawai',PegawaiController::class);
