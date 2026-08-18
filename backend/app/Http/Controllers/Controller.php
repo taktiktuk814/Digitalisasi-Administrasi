2c7a36277247474893f97e5114ea8992da347caa
taktiktuk814 <mfyulistiawan@gmail.com>
2026-08-18 23:01:04 +0700

    Update Controller.php

--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
 
-abstract class Controller extends BaseController
+abstract class Controller
 {
-    //
+    use AuthorizesRequests, ValidatesRequests;
 }
