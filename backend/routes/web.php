9aa4c45b2d4186d272c50cd8a80fd36f0e3c97ef
taktiktuk814 <mfyulistiawan@gmail.com>
2026-08-18 21:21:19 +0700

    Aktifkan routing seluruh modul SIMAD

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,14 @@
 <?php
-
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\AsetController;
+use App\Http\Controllers\SuratMasukController;
+use App\Http\Controllers\SuratKeluarController;
+use App\Http\Controllers\AgendaController;
+use App\Http\Controllers\ArsipController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', fn () => redirect()->route('aset.index'));
-Route::resource('aset', AsetController::class)->except(['show']);
+Route::get('/',[DashboardController::class,'index'])->name('dashboard');
+Route::resource('aset',AsetController::class)->except(['show']);
+Route::resource('surat-masuk',SuratMasukController::class)->except(['show']);
+Route::resource('surat-keluar',SuratKeluarController::class)->except(['show']);
+Route::resource('agenda',AgendaController::class)->except(['show']);
+Route::resource('arsip',ArsipController::class)->except(['show']);
